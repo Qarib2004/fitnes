6bc09453cc555ac78b91ac58c4b26c88bc2b55dc
Qarib2004 <garibalisultanov@gmail.com>
2026-06-03 14:14:11 +0400

    update drizzle config

--- fitnes-backend/drizzle.config.ts
+++ fitnes-backend/drizzle.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!
+    url: process.env.DATABASE_URL!,
+    ssl: true,
   }
 })
